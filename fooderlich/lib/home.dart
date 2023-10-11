205b1f9d0cbc508264fcb2a6f0ae5291ca2502a2
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-11 16:48:49 +0100

    Added images to the Assests folder and created BottomNavigationBar for my App

---
+++ fooderlich/lib/home.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/card1.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // TODO: Add state variables and function
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = [
+    // TODO: Replace with Card1
+    const Card1(),
+    // TODO: Replace with Card2
+    Container(
+      color: Colors.green,
+    ),
+    // TODO: Replace with Card3
+    Container(
+      color: Colors.blue,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Fooderlich',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+      ),
+      // TODO: Show selected tab
+      body: pages[_selectedIndex],
+      //TODO: Add bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // TODO: Set seleected tab bar
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+    );
+  }
+}
