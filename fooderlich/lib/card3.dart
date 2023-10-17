89136c75c963ef7abb94dcf17e3cc3e05fd9609a
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-17 09:49:01 +0100

    Added recipe trends

--- fooderlich/lib/card3.dart
+++ fooderlich/lib/card3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
 
 class Card3 extends StatelessWidget {
   const Card3({super.key});
@@ -18,17 +19,73 @@ class Card3 extends StatelessWidget {
           ),
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
-        child: Stack(children: [
-          //  TODO 5: add dark overlay BoxDecoration
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.6),
-              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+        child: Stack(
+          children: [
+            //  TODO 5: add dark overlay BoxDecoration
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+              ),
             ),
-          ),
-          //  TODO 6: Add Container, Column, Icon and Text
-          //  TODO 7: Add Center widget with Chip widget children
-        ]),
+            //  TODO 6: Add Container, Column, Icon and Text
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Recipe Trends',
+                    style: FooderlichTheme.darkTextTheme.displayMedium,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  )
+                ],
+              ),
+            ),
+            Center(
+              child: Wrap(
+                alignment: WrapAlignment.start,
+                spacing: 12,
+                children: [
+                  Chip(
+                    label: Text('Healty',
+                        style: FooderlichTheme.darkTextTheme.bodyLarge),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                  Chip(
+                    label: Text('Vegan',
+                        style: FooderlichTheme.darkTextTheme.bodyLarge),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                  Chip(
+                    label: Text('Carrots',
+                        style: FooderlichTheme.darkTextTheme.bodyLarge),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
