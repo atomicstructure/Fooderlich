27d46fa2a01f6d1c81a0bd3890df7791b59bc533
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-31 15:30:34 +0100

    Managed state to go to the next screen with the provider package

--- fooderlich/lib/main.dart
+++ fooderlich/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
 import 'home.dart';
+import 'models/models.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const Fooderlich());
@@ -14,7 +16,12 @@ class Fooderlich extends StatelessWidget {
     return MaterialApp(
       theme: theme,
       title: 'Fooderlich',
-      home: const Home(),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => TabManager()),
+        ],
+        child: const Home(),
+      ),
     );
   }
 }
