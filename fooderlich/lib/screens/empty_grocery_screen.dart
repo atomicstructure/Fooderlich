d7b379f80973ed32b7e46187279c7d0e21dd95d5
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-31 13:36:35 +0100

    provider for tab manager package installed

--- fooderlich/lib/screens/empty_grocery_screen.dart
+++ fooderlich/lib/screens/empty_grocery_screen.dart
@@ -6,6 +6,48 @@ class EmptyGroceryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Replace and add layout widgets
-    return Container(color: Colors.purple);
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // TODO: Add empty image
+            Flexible(
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+              ),
+            ),
+            // TODO: Add empty screen title
+            Text(
+              'No Groceries',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            // TODO: Add empty screen subtitle
+            const Text(
+              'Shopping for Ingredients?\n'
+              'Tap the + button to write them down!',
+              textAlign: TextAlign.center,
+            ),
+            // TODO: Add browse recipes button
+            MaterialButton(
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              color: Colors.green,
+              onPressed: () {
+                // TODO: Go to Recipes Tab
+              },
+              child: const Text('Browse Recipes'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
