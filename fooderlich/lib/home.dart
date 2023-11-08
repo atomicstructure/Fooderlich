004124c1e9db3a0ca8a6212f0526b292e1625066
Ogundare Olusesi Oluwafemi <ogundarearold@gmail.com>
2023-11-08 14:12:57 +0100

    Catched the pages

--- fooderlich/lib/home.dart
+++ fooderlich/lib/home.dart
@@ -32,7 +32,10 @@ class HomeState extends State<Home> {
           ),
         ),
         // TODO: Replace body
-        body: pages[tabManager.selectedTab],
+        body: IndexedStack(
+          index: tabManager.selectedTab,
+          children: pages,
+        ),
         bottomNavigationBar: BottomNavigationBar(
           selectedItemColor:
               Theme.of(context).textSelectionTheme.selectionColor,
