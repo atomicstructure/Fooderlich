e2eedc5475ddfc6c755eb2dcf7cf570b14c17acb
Ogundare Olusesi Oluwafemi <ogundarearold@gmail.com>
2023-11-08 13:53:31 +0100

    Used the Gesture widget(Inkwell)

--- fooderlich/lib/screens/grocery_list_screen.dart
+++ fooderlich/lib/screens/grocery_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fooderlich/components/grocery_tile.dart';
 import 'package:fooderlich/models/models.dart';
+import 'package:fooderlich/screens/grocery_item_screen.dart';
 
 class GroceryListScreen extends StatelessWidget {
   const GroceryListScreen({super.key, required this.manager});
@@ -20,13 +21,29 @@ class GroceryListScreen extends StatelessWidget {
           final item = groceryItems[index];
           // TODO: Wrap in a Dismissable
           // TODO: Wrap in an InkWell
-          return GroceryTile(
-            key: Key(item.id),
-            item: item,
-            onComplete: (change) {
-              if (change != null) {
-                manager.completeItem(index, change);
-              }
+          return InkWell(
+            child: GroceryTile(
+              item: item,
+              key: Key(item.id),
+              onComplete: (change) {
+                if (change != null) {
+                  manager.completeItem(index, change);
+                }
+              },
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GroceryItemScreen(
+                      originalItem: item,
+                      onCreate: (item) {},
+                      onUpdate: (item) {
+                        manager.updateItem(item, index);
+                        Navigator.pop(context);
+                      }),
+                ),
+              );
             },
           );
         },
